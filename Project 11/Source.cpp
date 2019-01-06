2971403e9c31982150a0e016228133d6b73cfad7
zozrom <zozulia.roman135@gmail.com>
2019-01-06 16:36:18 +0200

    Third commit!

--- Project 11/Source.cpp
+++ Project 11/Source.cpp
@@ -1,16 +1,34 @@
 #include<iostream>
 #include<cstdio>
 using namespace std;
+int calculation(char *str);
 int main()
 { //Counting the number of letters in the text
 	
 	 setlocale(LC_ALL, "ru");
-		char str[1000];
-			cout << "Enter the some text please :";
-				cout << endl;
-					gets_s(str);
-						cout << endl;
-						cout << "This text have " << "letters";
-	return 0;
+		char str[1000]; 
+		    
+			cout << "Enter the some text (on English, without symbols) please :";
+			cout << endl;
+				gets_s(str);
+				 cout << endl;  
+					int x;
+						x = calculation(str);
+							cout << "This text have " << x << " letters"; 
+
+									return 0;
 	 
+ }
+int calculation(char *str) {
+	int n = 0;
+	int i;
+	for (  i = 0; i < str[i]; i++)
+	{
+		if (str[i]==' ')
+		{
+			n++;
+		}
+
+	}
+	return i - n;
  }
