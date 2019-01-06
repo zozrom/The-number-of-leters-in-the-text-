3c56fc4cf1f0dbc6371aae3844854cf6ac2414cf
zozrom <zozulia.roman135@gmail.com>
2019-01-06 15:58:34 +0200

    Second commit!

--- Project 11/Source.cpp
+++ Project 11/Source.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
+#include<cstdio>
 using namespace std;
 int main()
-{  
-
-	//Counting the number of letters in the text
-
+{ //Counting the number of letters in the text
+	
+	 setlocale(LC_ALL, "ru");
+		char str[1000];
+			cout << "Enter the some text please :";
+				cout << endl;
+					gets_s(str);
+						cout << endl;
+						cout << "This text have " << "letters";
 	return 0;
 	 
  }
